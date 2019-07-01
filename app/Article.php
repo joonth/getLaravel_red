426d9fc98d3f8c07ad6a5ce2a827bb85dcd2d0ea
joonth <joonthy@gmail.com>
2019-07-01 21:48:47 +0900

    데이터베이스 시딩

--- app/Article.php
+++ app/Article.php
@@ -1,1 +1,18 @@
-App\
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    protected $fillable = ['title','content'];
+
+    public function user(){
+        return $this -> belongsTo(User::class);
+    }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+         if(config('database.default') !== 'sqlite'){
+            DB::statement('SET FOREIGN_KEY_CHECKS=0');
+         }
+
+     //    Model::unguard();
+
+         App\User::truncate();
+         $this->call(UsersTableSeeder::class);
+
+         App\Article::truncate();
+         $this->call(ArticlesTableSeeder::Class);
+
+     //   Model::reguard();
+
+        if(config('database.default') !== 'sqlite'){
+            DB::statement('SET FOREIGN_KEY_CHECKS=1');
+        }
+
+
+
     }
 }
