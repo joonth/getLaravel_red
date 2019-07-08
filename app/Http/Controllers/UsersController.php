962efbb084627eb4ff7008b10cf5d576058059f8
joonth <joonthy@gmail.com>
2019-07-08 21:30:22 +0900

    사용자 등록 구현 / 사용자 등록폼,사용자 등록 요청 처리

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+4
+class UsersController extends Controller
+{
+   public function create(){
+       return view('users.create');
+   }
+
+   public function store(Request $request){
+       $this ->validate($request,[
+          'name'=>'required|max:255',
+          'email'=>'required|email|max:255|unique:users',
+          'password' =>'required|confirmed|min:6',
+       ]);
+
+       $user = \App\User::create([
+          'name' =>$request->input('name'),
+          'email' => $request->input('email'),
+          'password' => bcrypt($request->input('password')),
+       ]);
+
+       auth()->login($user);
+       flash(auth()->user()->name . '님 환영합니다.');
+
+       return redirect('home');
+   }
+}
--- routes/web.php
+++ routes/web.php
@@ -135,3 +135,64 @@ EOT;
 Route::get('docs/{file?}','DocsController@show');
 
 Route::get('docs/images/{image}','DocsController@image')->where('image','[\pL-\pN\._-]+-img-[0-9]{2}.png');
+
+// Route::auth();
+
+
+
+
+/* 사용자 가입 */
+Route::get('auth/register',[
+   'as' => 'users.create',
+   'uses' => 'UsersController@create'
+]);
+
+Route::post('auth/register',[
+   'as' => 'users.store',
+   'uses' => 'UsersController@store'
+]);
+
+Route::get('auth/confirm/{code}',[
+   'as' => 'users.confirm',
+   'uses' => 'UsersController@confirm'
+]);
+
+/* 사용자 인증 */
+
+Route::get('auth/login',[
+   'as' => 'sessions.create',
+   'uses' => 'SessionsController@create'
+]);
+
+Route::post('auth/login',[
+   'as' => 'sessions.store',
+   'uses' => 'SessionsController@store'
+]);
+
+Route::get('auth/logout',[
+    'as' => 'sessions.destory',
+    'uses' => 'SessionsController@destroy'
+]);
+
+
+/*  비밀번호 초기화  */
+
+Route::get('auth/remind',[
+   'as' => 'remind.create',
+   'uses'=> 'PasswordsController@getRemind',
+]);
+
+Route::post('auth/remind',[
+   'as' => 'remind.store',
+    'uses' => 'PasswordsController@postRemind',
+]);
+
+Route::get('auth/reset/{token}',[
+   'as' => 'reset.create',
+   'uses' => 'PasswordsController@getReset',
+]);
+
+Route::post('auth/reset',[
+   'as' => 'reset.store',
+   'uses' => 'PasswordsController@postReset',
+]);
