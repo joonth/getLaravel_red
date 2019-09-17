01bc57523a0e76471f64cea91f1c36f7fb70554b
joonth <joonthy@gmail.com>
2019-09-17 20:22:04 +0900

    라우팅

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,15 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/',[
+   'as'=>'home',
+   function(){
+    return 'nanana';
+   }
+]);
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/home',function (){
+   return redirect(route('home'));
 });
+
